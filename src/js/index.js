27ae31f3b4de114a113e2f54d2046a4c506e5bd9
tinchin40 <tinchin40@gmail.com>
2022-06-13 22:52:56 +0800

    no message

---
+++ src/js/index.js
@@ -0,0 +1,20 @@
+// 影片滾動播放
+$(document).scroll(function () {
+
+    var topnuml = $("#index-youtube").offset().top - $(document).scrollTop();
+    // var bottomnuml = window.innerHeight - (topnuml + $("#index-youtube").height());
+    if ($(document).scrollTop() >  ($("#index-youtube").offset().top -  window.innerHeight)) {
+        $("#index-youtube").get(0).play();
+
+
+    } 
+    
+    
+    else {
+
+        $("#index-youtube").get(0).pause();
+    }
+console.log(topnuml)
+console.log($(document).scrollTop())
+// console.log(bottomnuml);
+});
