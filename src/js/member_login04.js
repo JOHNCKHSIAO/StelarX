992ce494dda7f936ed51c28ab8bc78b59f3f4e96
tinchin40 <tinchin40@gmail.com>
2022-06-14 16:16:52 +0800

    製作會員頁面04進度條動畫 修改聯絡我們 首頁的按鈕

--- src/js/member_login04.js
+++ src/js/member_login04.js
@@ -2,7 +2,9 @@ $(document).ready(function () {
     $('.mileCardUp').click(function () {
         $(this).parent().find('.mileCardDown').slideToggle(200);
         $(this).parent().siblings().find('.mileCardDown').slideUp(200)
+        $(".g-progress").css("width","300");
     })
+  
  
 
   
