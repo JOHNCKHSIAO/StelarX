eaa82eaf428482f980bb5dfbda9380f8f0083b56
tinchin40 <tinchin40@gmail.com>
2022-07-02 16:15:04 +0800

    add

---
+++ src/js/member_login04_vue.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+    $('.mileCardUp').click(function () {
+        $(this).parent().find('.mileCardDown').slideToggle(200);
+        $(this).parent().siblings().find('.mileCardDown').slideUp(200)
+        $(".g-progress").css("width", "300");   //進度條動畫
+
+    })
+
+
+});
+$(window).scroll(function () {
+    var scroll = $(this).scrollTop()
+    if (scroll >= $('.mainMile').position().top - 100) {
+        $('.bgsvg').css("height", 700); //線條動畫
+
+    }
+
+});
+new Vue({
+    el: '#login04', //
+
+    data: {
+        
+    },
+    methods: {
+        card_click(){
+            $(this).parent().find('.mileCardDown').slideToggle(200);//open動畫
+            $(this).parent().siblings().find('.mileCardDown').slideUp(200)
+            $(".g-progress").css("width", "300");   //進度條動畫
+        }
+    },
+    compute: {},
+    watch: {},
+    created() {
+    },
+    mounteds(){},
+    destroyed(){},
+
+})
